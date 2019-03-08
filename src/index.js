44d0ac7f62b9218d5863ff92422b110f292368ab
Айнур-ПК\Айнур <felix-xilef@mail.ru>
2019-03-08 13:34:40 +0300

    add factorial calculation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function getZerosCount(number, base) {
-  // your implementation
+var factorial = function(n) {
+  if (n != 1) {
+    return n * factorial(n - 1);
+  } else {
+    return 1;
+  }
 }
+
+module.exports = function getZerosCount(number, base) {
+  //  вычисляю факториал
+  var factorialResult = factorial(n);
+};
