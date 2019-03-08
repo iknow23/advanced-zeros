950f4f5fa325299a5e5bef3c0f03172e310033dc
Айнур-ПК\Айнур <felix-xilef@mail.ru>
2019-03-08 13:40:16 +0300

    Fix counts zeros count

--- src/index.js
+++ src/index.js
@@ -7,7 +7,20 @@ var factorial = function(n) {
 }
 
 module.exports = function getZerosCount(number, base) {
+  //  счётчик нулей
+  var count = 0;
+  
   //  вычисляю факториал
   var factorialResult = factorial(n);
   factorialResult = '' + factorialResult;
+
+  //  считаю кол-во нулей
+  for (var i = (factorialResult.length - 1); i >= 0; i--) {
+    if (factorialResult[i] == 0) {
+      count++;
+    } else {
+      count = count;
+    }
+  }
+  return count;
 };
